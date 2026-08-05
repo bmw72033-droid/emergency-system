61f447e6cfa5c4a177f019ca4f7c0830d7bb65bb
bmw72033-droid <bmw72033@gmail.com>
2026-08-05 22:58:42 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://ezirapbynmyndvtwrefj.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ezirapbynmyndvtwrefj.supabase.co";
 const SUPABASE_KEY = "sb_publishable_vROJEoA2pQ-9OHMUUCgLvA_8X_Ss_Rb";
 
 const supabaseClient = supabase.createClient(
