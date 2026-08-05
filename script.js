fd208dea772d6887ae028669e180937f2d826f9a
bmw72033-droid <bmw72033@gmail.com>
2026-08-05 22:52:50 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const SUPABASE_URL = "https://ezirapbynmyndvtwrefj.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_vROJEoA2pQ-9OHMUUCgLvA_8X_Ss_Rb";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+async function sendReport(){
+
+    const description = document.getElementById("description").value;
+
+    const { data, error } = await supabaseClient
+    .from("reports")
+    .insert([
+        {
+            description: description,
+            status: "new"
+        }
+    ]);
+
+    if(error){
+        alert("Error sending report");
+        console.log(error);
+    }
+    else{
+        alert("Emergency report sent!");
+    }
+
+}
